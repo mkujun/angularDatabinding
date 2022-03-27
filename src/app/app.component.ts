b7f2334a5cc99b1f03cabd3f01aad16f553efbd4
= <=>
2022-03-27 18:07:11 +0200

    databinding excercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular_databinding';
+  propertyBindingVar: string  = 'property binding test';
+  stringInterpolationVar : string = "string interpolation test";
+
+  username: string = "";
+
+  pressFun() : void {
+    console.log("button pressed...");
+  }
+
+  buttonTitle : string = "default button title";
+
+  changeButtonTitle() : void {
+    this.buttonTitle = "new button title";
+  }
+
+  onUpdateName(event : any) : void {
+    this.username = event.target.value;
+    if (this.username.length > 0) {
+      this.nameDisabled = false;
+    }
+    else {
+      this.nameDisabled = true;
+    }
+  }
+
+  onReset() : void {
+    this.username = "";
+    this.nameDisabled = true;
+  }
+
+  nameDisabled : boolean = true;
 }
